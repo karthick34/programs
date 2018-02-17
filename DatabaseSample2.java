997a345750f9fd3841a3f6a66c5f8b098765743e
yogeshfromchennai <yogeshsoagesh@gmail.com>
2018-02-17 11:30:15 +0530

    ds

---
+++ DatabaseSample2.java
@@ -0,0 +1,21 @@
+package com.jdbc;
+
+import java.util.Scanner;
+
+public class DatabaseSample2 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		while(scanner.hasNext()){
+			
+			
+			System.out.println("Enter the value"+ scanner.next());
+		}
+		
+		
+	}
+	
+}
